786ab9a9f166b736fa8bf1102d340286a5d24512
JeanleeRoy <jeanlee.barreto@utec.edu.pe>
2019-08-25 20:40:45 -0500

    Quotes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include<string.h>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    char str[100000];
+    long count = 0;
+    while(gets(str)){
+        long l = strlen(str);
+        for(int i = 0; i < l; i++) {
+            if(str[i]=='"') {
+                count++;
+                if (count%2 == 1) cout << "``";
+                else cout << "''";
+            } else cout << str[i];
+        }
+        cout << endl;
+    }
     return 0;
 }
